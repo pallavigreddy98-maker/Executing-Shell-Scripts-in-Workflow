b00f2b18551f6aebbaa508835fe63975bb12c81f
pallavireddy <pallavigreddy98@gmail.com>
2026-05-19 19:02:44 +0530

    updated separate shell script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,5 @@ steps:
   - name: Checkout code
     uses: actions/checkout@v3
 
-  - name: Run shell script
-    run: chmod +x setup.sh && ./setup.sh
-
   - name: Run simple shell command
     run: echo "Hello from shell!"
